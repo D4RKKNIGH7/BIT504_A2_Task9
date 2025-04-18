81aca6396eb7bc241ce04711269cf352a18c98f6
D4RKKNIGH7 <nignogster@gmail.com>
2025-04-18 12:12:07 +1200

    Added a SendIntroMessage method Fixed missing ';' at print lines

--- src/task9/Main.java
+++ src/task9/Main.java
@@ -2,14 +2,20 @@ package task9;
 
 public class Main {
 
-	public static void main(String[] args) {
-		print("Hello World");
-		print("This is my print method")
-		print("Thank you for viewing")
+	public static void main(String[] args) 
+	{
+		SendIntroMessage();
 	}
 	
 	private static void print(String message)
 	{
 		System.out.println(message);
+	}
+	
+	private static void SendIntroMessage()
+	{
+		print("Hello World");
+		print("This is my print method");
+		print("Thank you for viewing");
 	}
 }
